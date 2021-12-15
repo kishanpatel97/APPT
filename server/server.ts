a13034b385d96f6549caa13cdcfc8156d063bce4
Kishan Patel <kishapat@cisco.com>
2021-12-15 14:55:47 -0800

    first server.ts commit

---
+++ server/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import cors from 'cors';
+import './routes/appt.routes';
+import './config/mongoose.config';
+const app = express();
+const port = 8000;
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.listen(port, () => console.log(`Listening on port: ${port}`));
