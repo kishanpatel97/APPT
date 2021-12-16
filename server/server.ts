e3601fc2013c065b779406fc977ed807c2615b57
Brandon Chang <brandochang1993@gmail.com>
2021-12-15 23:47:42 -0800

    set up mongoose config to connect to local db

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import './routes/appt.routes';
-import './config/mongoose.config';
+import connectDB from './config/mongoose.config';
 const app = express();
 const port = 8000;
 app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+connectDB();
+
 app.listen(port, () => console.log(`Listening on port: ${port}`));
