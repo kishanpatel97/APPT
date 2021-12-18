e2402f393c4bed3b346cf26dfb2a1e2f80a6e424
Brandon Chang <brandochang1993@gmail.com>
2021-12-18 08:13:43 -0800

    change to .json in appt controller

--- server/controllers/appt.controller.ts
+++ server/controllers/appt.controller.ts
@@ -5,43 +5,43 @@ import { IAppointment } from "../interfaces/appointment.interface";
 const findAllAppointments = async (req: Request, res: Response): Promise<IAppointment[]> => {
   try {
     const appointments = await Appointment.find()
-    return res.status(200).send(appointments)
+    return res.status(200).json(appointments)
   } catch(e) {
-    return res.status(500).send(e.message)
+    return res.status(500).json(e.message)
   }
 }
 
 const createNewAppointment = async (req: Request, res: Response): Promise<IAppointment> => {
   try {
     const appointment = await Appointment.create(req.body)
-    return res.status(201).send(appointment)
+    return res.status(201).json(appointment)
   } catch(e) {
-    return res.status(500).send(e.message)
+    return res.status(500).json(e.message)
   }
 }
 
 const findOneAppointment = async (req: Request, res: Response): Promise<IAppointment> => {
   try {
     const appointment = await Appointment.findOne({ _id: req.params.id })
-    return res.status(200).send(appointment)
+    return res.status(200).json(appointment)
   } catch(e) {
-    return res.status(500).send(e.message)
+    return res.status(500).json(e.message)
   }
 }
 const updateAppointment = async (req: Request, res: Response): Promise<IAppointment> => {
   try {
     const appointment = await Appointment.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
-    return res.status(200).send(appointment)
+    return res.status(200).json(appointment)
   } catch(e) {
-    return res.status(500).send(e.message)
+    return res.status(500).json(e.message)
   }
 }
 const deleteAppointment = async (req: Request, res: Response): Promise<{}> => {
   try {
     const appointment = await Appointment.deleteOne({ _id: req.params.id })
-    return res.status(200).send(appointment)
+    return res.status(200).json(appointment)
   } catch(e) {
-    return res.status(500).send(e.message)
+    return res.status(500).json(e.message)
   }
 }
 
